35eac57af9cabc046c33cd3efb76d00eb6f15ce1
Terry-Martin <terry.martin@gmail.com>
2023-03-28 19:25:18 +0000

    Add main method. Edit attack method

--- run.py
+++ run.py
@@ -25,28 +25,12 @@ class Character:
         self.health = health
         self.attack_power = attack_power
 
-    def attack(self, p_name, p_attack_power, p_health, f_name,
-               f_attack_power, f_health):
+    def attack2(self, health, attack_power):
         """
-        ATTACK
+        New attack
         """
-        while f_health > 0 and p_health > 0:
-
-            print(f"{p_name} has dealt {p_attack_power} damage")
-            f_health -= p_attack_power
-            print(f"{f_name} has {f_health} health remaining\n")
-
-            if f_health <= 0:
-                print(f"{p_name} has defeated the {f_name}!!!")
-                break
-
-            print(f"{f_name} has dealt {f_attack_power} damage")
-            p_health -= f_attack_power
-            print(f"{p_name} has {p_health} health remaining\n")
-
-            if p_health <= 0:
-                print(f"The {f_name} has defeated {p_name}!!!")
-                break
+        health -= attack_power
+        return health
 
     def intro(self, cid, name, health, attack_power):
         """
@@ -64,13 +48,10 @@ player_id = player_info_all.cell(2, 1).value
 player_name = player_info_all.cell(2, 2).value
 player_health = int(player_info_all.cell(2, 3).value)
 player_attack_power = int(player_info_all.cell(2, 4).value)
-
 player = Character(player_id, player_name, player_health, player_attack_power)
 
-player.intro(player.cid, player.name, player.health, player.attack_power)
-
 foe_info_all = SHEET.worksheet("foe")
-FOE_TYPE = 5
+FOE_TYPE = 9
 foe_id = foe_info_all.cell(FOE_TYPE, 1).value
 foe_name = foe_info_all.cell(FOE_TYPE, 2).value
 foe_health = int(foe_info_all.cell(FOE_TYPE, 3).value)
@@ -78,7 +59,34 @@ foe_attack_power = int(foe_info_all.cell(FOE_TYPE, 4).value)
 
 foe = Character(foe_id, foe_name, foe_health, foe_attack_power)
 
-foe.intro(foe.cid, foe.name, foe.health, foe.attack_power)
 
-player.attack(player.name, player.attack_power, player.health, foe.name,
-              foe.attack_power, foe.health)
+def start_battle():
+    """
+    Start battle
+    """
+    while foe.health > 0 and player.health > 0:
+        print(f"{player.name} has dealt {player.attack_power} damage")
+        foe.health = player.attack2(foe.health, player.attack_power)
+        print(f"{foe.name} has {foe.health} health remaining\n")
+        if foe.health <= 0:
+            print(f"{player.name} has defeated the {foe.name}!!!")
+            break
+
+        print(f"{foe.name} has dealt {foe.attack_power} damage")
+        player.health = foe.attack2(player.health, foe.attack_power)
+        print(f"{player.name} has {player.health} health remaining\n")
+        if player.health <= 0:
+            print(f"The {foe.name} has defeated {player.name}!!!")
+            break
+
+
+def main():
+    """
+    Main function
+    """
+    player.intro(player.cid, player.name, player.health, player.attack_power)
+    foe.intro(foe.cid, foe.name, foe.health, foe.attack_power)
+    start_battle()
+
+
+main()
