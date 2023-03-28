c43f14b2b82914a2186cad5903d4d84eb15208e9
Terry-Martin <terry.martin@gmail.com>
2023-03-28 20:24:14 +0000

    Add start_battle function

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ SHEET = GSPREAD_CLIENT.open("takeover")
 
 class Character:
     """
-    Creat Character class
+    Create Character class to be used for player and foe
     """
     def __init__(self, cid, name, health, attack_power):
         self.cid = cid
@@ -25,31 +25,36 @@ class Character:
         self.health = health
         self.attack_power = attack_power
 
-    def attack2(self, health, attack_power):
+    def attack(self, health, attack_power):
         """
-        New attack
+        Attack is health minus attack_power
         """
         health -= attack_power
         return health
 
     def intro(self, cid, name, health, attack_power):
         """
-        Display introduction
+        Display information about character
         """
-        print(f"Hi, my name is {name}.")
-        print(f"I have the number {cid} tattoed on my arm.")
-        print(f"My health is at {health}.")
+        print(f"My name is {name}.")
+        print(f"The number {cid} is tattoed on my arm.")
+        print(f"My health is {health}.")
         print(f"My attack power is {attack_power}.\n")
 
 
+# Get information from google sheet for player
 player_info_all = SHEET.worksheet("player")
-# player_data = player_info_all.get_all_values()
+player_data = player_info_all.get_all_values()
+# player_data = SHEET.get_all_records()
 player_id = player_info_all.cell(2, 1).value
 player_name = player_info_all.cell(2, 2).value
 player_health = int(player_info_all.cell(2, 3).value)
 player_attack_power = int(player_info_all.cell(2, 4).value)
+
+# Create player from Character class
 player = Character(player_id, player_name, player_health, player_attack_power)
 
+# Get information from google sheet for foe
 foe_info_all = SHEET.worksheet("foe")
 FOE_TYPE = 9
 foe_id = foe_info_all.cell(FOE_TYPE, 1).value
@@ -57,27 +62,38 @@ foe_name = foe_info_all.cell(FOE_TYPE, 2).value
 foe_health = int(foe_info_all.cell(FOE_TYPE, 3).value)
 foe_attack_power = int(foe_info_all.cell(FOE_TYPE, 4).value)
 
+# Create foe from Chatacter class
 foe = Character(foe_id, foe_name, foe_health, foe_attack_power)
 
 
 def start_battle():
     """
-    Start battle
+    Start battle between player and foe
     """
     while foe.health > 0 and player.health > 0:
         print(f"{player.name} has dealt {player.attack_power} damage")
-        foe.health = player.attack2(foe.health, player.attack_power)
+        foe.health = player.attack(foe.health, player.attack_power)
         print(f"{foe.name} has {foe.health} health remaining\n")
         if foe.health <= 0:
             print(f"{player.name} has defeated the {foe.name}!!!")
             break
 
         print(f"{foe.name} has dealt {foe.attack_power} damage")
-        player.health = foe.attack2(player.health, foe.attack_power)
+        player.health = foe.attack(player.health, foe.attack_power)
         print(f"{player.name} has {player.health} health remaining\n")
         if player.health <= 0:
             print(f"The {foe.name} has defeated {player.name}!!!")
             break
+
+
+def update_worksheet(data, worksheet):
+    """
+    Update worksheet, add new row with list data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully. \n")
 
 
 def main():
@@ -87,6 +103,8 @@ def main():
     player.intro(player.cid, player.name, player.health, player.attack_power)
     foe.intro(foe.cid, foe.name, foe.health, foe.attack_power)
     start_battle()
+    new_surplus_data = ["One", "One", "One", "One"]
+    update_worksheet(new_surplus_data, "player")
 
 
 main()
