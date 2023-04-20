cea1571c4feaa63e8d4de88b90cdc863176091d5
Terry-Martin <terry.martin@gmail.com>
2023-04-20 12:55:24 +0000

    Check through code and tidy/format

--- run.py
+++ run.py
@@ -24,16 +24,13 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("takeover")
-DELAY_1 = 1
-DELAY_2 = 2
-DELAY_3 = 3
-DELAY_4 = 4
 
 
 class Character:
     """
     Create Character class to be used for player and foe
     """
+    
     def __init__(self, cid, name, health, attack_power):
         self.cid = cid
         self.name = name
@@ -44,6 +41,7 @@ class Character:
         """
         Attack is health minus attack_power
         """
+
         health -= attack_power
         return health
 
@@ -51,19 +49,20 @@ class Character:
         """
         Display information about character
         """
-        time.sleep(DELAY_1)
+
+        time.sleep(1)
         print(f"  Your name is {name}.")
         print(f"  {cid}Your current health is {health}.")
         print(f"  Your attack power is {attack_power}.\n")
-        time.sleep(DELAY_1)
+        time.sleep(1)
 
 
 def create_player(new_name):
     """
     Create player
     """
-    # Get information from google sheet for player
 
+    # Get current information from google sheet for player
     get_player_info = SHEET.worksheet("player").col_values(1)
 
     # Get new id number which is 1 higher than previous id number
@@ -85,8 +84,8 @@ def create_foe(foe_number):
     """
     Create foe
     """
-    # Get information from google sheet for foe
 
+    # Get information from google sheet for which ever foe number is passed in
     get_foe_info = SHEET.worksheet("foe").row_values(foe_number)
     foe_id = get_foe_info[0]
     foe_name = get_foe_info[1]
@@ -99,16 +98,21 @@ def create_foe(foe_number):
 
 def game_over():
     """
-    Format game over text
+    End game display
     """
+
+    # Format text
     result = pyfiglet.figlet_format("              GAME OVER")
+    print(Fore.WHITE)
     print(Back.RED)
     print(result)
     print(Style.RESET_ALL)
     print(Fore.WHITE)
-    time.sleep(DELAY_3)
+    time.sleep(3)
+
+    # Restart app
     print("Restarting game.....")
-    time.sleep(DELAY_3)
+    time.sleep(4)
     main()
 
 
@@ -116,8 +120,10 @@ def start_battle(foe, player):
     """
     Start battle between player and foe
     """
-    while foe.health > 0 and player.health > 0:
 
+    # Loop through until either player or foe is defeated
+    while foe.health > 0 and player.health > 0:
+        # Display damage taken for each attack and remaining health points
         print(Fore.RED + f"  {player.name} has dealt {player.attack_power}" +
               " damage")
         print(Fore.WHITE)
@@ -125,9 +131,12 @@ def start_battle(foe, player):
         print(f"  {foe.name} has {foe.health} health remaining\n")
         if foe.health <= 0:
             print(f"  {player.name} has defeated the {foe.name}!!! \n")
-            time.sleep(DELAY_2)
+            time.sleep(2)
+            # Foe defeated and break loop
+            # If there is another foe in this battle, it will start next
             break
 
+        # Display damage taken for each attack and remaining health points
         print(f"  {foe.name} has dealt {foe.attack_power} damage")
         player.health = foe.attack(player.health, foe.attack_power)
         print(Fore.GREEN + f"  {player.name} has {player.health}" +
@@ -135,7 +144,8 @@ def start_battle(foe, player):
         print(Fore.WHITE)
         if player.health <= 0:
             print(f"  The {foe.name} has defeated {player.name}!!! \n")
-            time.sleep(DELAY_1)
+            time.sleep(2)
+            # Player defeated and game over
             game_over()
 
 
@@ -143,7 +153,9 @@ def story_intro(story_start):
     """
     Display story start
     """
+
     # https://pypi.org/project/colorama/
+    # Format text
     result = pyfiglet.figlet_format("              TakeOver")
     print(Back.BLUE)
     print(Style.BRIGHT)
@@ -151,34 +163,31 @@ def story_intro(story_start):
     print(Style.RESET_ALL)
     print(Fore.WHITE)
 
-    # print(Back.GREEN + 'and with a green background')
-    # print(Style.DIM + 'and in dim text')
-    # print(Style.RESET_ALL)
-    # print(Style.BRIGHT + 'and in bright text')
-    # print(Style.RESET_ALL)
-    # print(Back.BLUE)
-
     # Time delay idea from classmate Darragh Lynch
-    time.sleep(DELAY_1)
+    time.sleep(1)
+    # Display story intro
+    # Text pulled from "text" google sheet
     print("\n" + story_start[1] + "\n")
-    time.sleep(DELAY_2)
+    time.sleep(2)
     print(story_start[2] + "\n")
-    time.sleep(DELAY_2)
+    time.sleep(2)
     print(story_start[3] + "\n")
-    time.sleep(DELAY_2)
+    time.sleep(2)
     print(story_start[4] + "\n")
 
 
 def decision(decision_tree, player):
     """
-    Testtesttest
+    Captures which branch/decision tree the player is currently at
     """
+    # Text pulled from "text" google sheet
     get_text_info = SHEET.worksheet("text").col_values(2)
 
+    # First decision tree for player to make selection
     if decision_tree == 1:
         while True:
             player_choice = input(get_text_info[7])
-
+            # Validate user entering a number
             try:
                 player_choice = int(player_choice)
                 break
@@ -186,10 +195,16 @@ def decision(decision_tree, player):
                 print("  You need to enter a number" + "\n")
 
         if player_choice == 1:
+            # Battle will have 3 foes
+
+            # Create first foe
             foe = create_foe(3)
+            # Display info on first foe
             foe.intro(foe.cid, foe.name, foe.health, foe.attack_power)
+            # Begin battle between first foe and player
             start_battle(foe, player)
 
+            # Begin battle with second foe
             foe_two = create_foe(3)
             foe_two.intro(foe_two.cid, foe_two.name, foe_two.health,
                           foe_two.attack_power)
@@ -200,6 +215,7 @@ def decision(decision_tree, player):
                             foe_three.attack_power)
             start_battle(foe_three, player)
         else:
+            # Battle will have 3 foes
             foe = create_foe(2)
             foe.intro(foe.cid, foe.name, foe.health, foe.attack_power)
             start_battle(foe, player)
@@ -213,14 +229,15 @@ def decision(decision_tree, player):
             foe_three.intro(foe_three.cid, foe_three.name, foe_three.health,
                             foe_three.attack_power)
             start_battle(foe_three, player)
-        time.sleep(DELAY_1)
+        time.sleep(1)
         print("\n" + get_text_info[8] + "\n")
-        time.sleep(DELAY_1)
+        time.sleep(1)
         print(get_text_info[9] + "\n")
-        time.sleep(DELAY_1)
+        time.sleep(1)
 
+    # Second decision tree for player to make selection
     elif decision_tree == 2:
-        # Validate player input choice
+        # Validate user entering a number
         while True:
             player_choice = input(get_text_info[10])
             try:
@@ -253,12 +270,13 @@ def decision(decision_tree, player):
             foe_three.intro(foe_three.cid, foe_three.name, foe_three.health,
                             foe_three.attack_power)
             start_battle(foe_three, player)
-        time.sleep(DELAY_1)
+        time.sleep(1)
         print("\n" + get_text_info[11] + "\n")
-        time.sleep(DELAY_1)
+        time.sleep(1)
         print(get_text_info[12] + "\n")
-        time.sleep(DELAY_1)
+        time.sleep(1)
 
+    # Third decision tree for player to make selection
     elif decision_tree == 3:
         # Validate player input choice
         while True:
@@ -278,36 +296,42 @@ def decision(decision_tree, player):
             foe.intro(foe.cid, foe.name, foe.health, foe.attack_power)
 
             # Reaction Time fight
+            # User hits enter when GO word is diplayed. Reaction time is noted
             print(get_text_info[14])
-            time.sleep(DELAY_1)
+            time.sleep(1)
             print(get_text_info[15])
-            time.sleep(DELAY_1)
+            time.sleep(1)
             print(get_text_info[16])
             print()
+            # Randon time for the word GO to appear between 2 and 5 seconds
             time.sleep(random.randint(2, 5))
             print(get_text_info[17])
             start_time = time.time()
             input()
             end_time = time.time()
+            # Player reaction time check against foe time
             player_reaction_time = end_time - start_time
             foe_reaction_time = 0.4
-            time.sleep(DELAY_1)
+            time.sleep(1)
+            # Display player and foe reaction times
             print(f"Your reaction time was {player_reaction_time}")
             print(f"{foe.name} reaction time was {foe_reaction_time}")
-            time.sleep(DELAY_1)
+            time.sleep(1)
 
+            # Foe defeated if player reaction time is faster
             if player_reaction_time <= foe_reaction_time:
                 print(f"You take out the {foe.name} with one clean hit")
                 foe.health = 0
                 print(Fore.GREEN + f"  {player.name} has {player.health}" +
                       " health remaining\n")
-                time.sleep(DELAY_1)
+                time.sleep(1)
+            # Player takes 200 damage if foe reaction time is faster
             else:
                 print(f"{foe.name} gets in a quick attack")
                 player.health = player.health - 200
                 print(Fore.GREEN + f"  {player.name} has {player.health}" +
                       " health remaining\n")
-                time.sleep(DELAY_1)
+                time.sleep(1)
                 if player.health <= 0:
                     game_over()
 
@@ -316,42 +340,43 @@ def boss_battle():
     """
     Final battle
     """
-    time.sleep(DELAY_1)
+
+    time.sleep(1)
     get_text_info = SHEET.worksheet("text").col_values(2)
     print(get_text_info[18] + "\n")
-    time.sleep(DELAY_1)
+    time.sleep(1)
 
 
 def main():
     """
-    Main function
+    Main function to start game
     """
+
+    # Format text and display
     print(Style.RESET_ALL)
     print(Fore.WHITE)
     get_text_info = SHEET.worksheet("text").col_values(2)
     story_intro(get_text_info)
 
+    # Prompt for user
     enter_name = input("  Enter username: ")
 
-    # create new played from character class
+    # create new played from character class based on inputted username
     player = create_player(enter_name)
     print()
 
-    time.sleep(DELAY_1)
+    time.sleep(1)
     print(get_text_info[5] + "\n")
-    time.sleep(DELAY_1)
+    time.sleep(1)
     print(get_text_info[6] + "\n")
 
-    # Validate player input choice
     # https://www.youtube.com/watch?v=LUWyA3m_-r0
-    # user_response1 = text_info_all.cell(4, 2).value
 
     decision(1, player)
     decision(2, player)
     decision(3, player)
     boss_battle()
     print(Style.RESET_ALL)
-    # exit()
     main()
 
 
